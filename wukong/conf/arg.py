88bbaa7687b00751279c0e359e2df75792a7d7a4
qiqilelebaobao <qiqilelebaobao@163.com>
2024-09-05 14:04:28 +0800

    feat: update command parse version.

--- wukong/conf/arg.py
+++ wukong/conf/arg.py
@@ -5,10 +5,13 @@ def init_arg():
     
     parser = argparse.ArgumentParser(prog='python3 exam.py', description='This is a exam program for student.', epilog='Enjoy the exam. :) ')
 
-    parser.add_argument('--level', '-l', help='level', default=1, type=int, choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    parser.add_argument('--level', '-l', help='level', default=1, type=int, choices=range(1, 10))
     parser.add_argument('--basic_cnt', '-bc', help='basic question count', default=25, type=int)
     parser.add_argument('--open_cnt', '-oc', help='open question count', default=0, type=int)
     parser.add_argument('--algorithm', '-a', help='algorithm', default='+', choices=['+', '-'])
+    parser.add_argument('--version', '-v', action='version')
+
+    parser.version = '1.0.0'
 
     args = parser.parse_args()
 
