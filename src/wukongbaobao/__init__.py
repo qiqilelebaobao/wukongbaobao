b913a185d799e2b388692f72257d1067ff9349c6
qiqilelebaobao <qiqilelebaobao@163.com>
2024-09-12 17:33:52 +0800

    version 1.0.7

--- src/wukongbaobao/__init__.py
+++ src/wukongbaobao/__init__.py
@@ -1,5 +1,5 @@
 '''Wukongbaobao is a program that helps parents flexibly generate test questions for their children. The tool is written in Python.'''
 
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 
 from . import mathematics
