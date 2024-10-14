a728abab135602b6758d14d0c937b4c9e9b07e62
qiqilelebaobao <qiqilelebaobao@163.com>
2024-10-14 14:29:54 +0800

    fix: remove unuse init

--- src/wukongbaobao/__init__.py
+++ src/wukongbaobao/__init__.py
@@ -1,5 +1,3 @@
 '''Wukongbaobao is a program that helps parents flexibly generate test questions for their children. The tool is written in Python.'''
 
 __version__ = '1.0.7'
-
-from . import mathematics
