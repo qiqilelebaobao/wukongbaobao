d077a7724f47a11ebd1f5e3283c8cc9c18a3ba9c
qiqilelebaobao <qiqilelebaobao@163.com>
2024-10-15 14:26:40 +0800

    fix: update entry

--- src/wukongbaobao/__main__.py
+++ src/wukongbaobao/__main__.py
@@ -7,27 +7,26 @@ from wukongbaobao.view.printer import Printer
 from wukongbaobao.conf.arg import init_arg
 from wukongbaobao.conf.output import init_output_dir
 
+def main():
+
+    level, basic_cnt, open_cnt, algorithm = init_arg()
+
+    init_output_dir()
+
+    m = Question()
+    qs = m.generate_questions(level, basic_cnt, open_cnt, algorithm)
+
+    p = Printer()
+    name = p.output(qs, 'docx')
+    
+    feedback_template = f'''
+Successfully generated "{name}" math tests:
+The level is {level}, basic question {basic_cnt}, open questions {open_cnt}.
+Please enjoy it.
+'''
+
+    print(feedback_template)
 
 if __name__ == '__main__':
-
-    def main():
-
-        level, basic_cnt, open_cnt, algorithm = init_arg()
-
-        init_output_dir()
-
-        m = Question()
-        qs = m.generate_questions(level, basic_cnt, open_cnt, algorithm)
-
-        p = Printer()
-        name = p.output(qs, 'docx')
-        
-        feedback_template = f'''
-    Successfully generated "{name}" math tests:
-    The level is {level}, basic question {basic_cnt}, open questions {open_cnt}.
-    Please enjoy it.
-    '''
-
-        print(feedback_template)
 
     main()
